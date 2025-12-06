385c5588aaa9da54f873d6fdd5d865402f4dd9bb
m-sadkowski <144620020+m-sadkowski@users.noreply.github.com>
2025-12-06 14:01:04 +0100

    update project structure (#5)

---
+++ Nawia/src/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <Game.h>
+
+using namespace Nawia;
+
+int main(int argc, char* argv[]) {
+    Core::Game game;
+
+    if (game.isRunning()) {
+        game.run();
+    }
+
+    return 0;
+}
